7bbd33ce1a8ec74c5469c65ade6551890a647865
yoh91asakura <fix113@hotmail.com>
2014-03-01 19:29:20 +0100

    Petit remaniement d'achitecture, les entités sont maintenent rengeinées dans le TennisBookCoreBundle
    Installation du doctrinefixturesBundle
    Installation du FSOUSerBundle
    
    Mise en place de l'entité annonces
    
    début des tests sur l'authentification

--- src/TennisBook/AccueilBundle/Controller/AccueilController.php
+++ src/TennisBook/AccueilBundle/Controller/AccueilController.php
@@ -3,11 +3,23 @@
 namespace TennisBook\AccueilBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use TennisBook\CoreBundle\Entity\Annonce;
 
 class AccueilController extends Controller
 {
     public function indexAction()
     {
         return $this->render('TennisBookAccueilBundle:Accueil:index.html.twig');
+    }
+
+    public function indexProAction()
+    {
+        $em = $this->container->get('doctrine.orm.entity_manager');
+        $annonces = $em->getRepository('TennisBookCoreBundle:Annonce')->getRecentAnnonces();
+
+        foreach($annonces as $annonce) {
+            $annonce->setDate($annonce->getDate()->format('Y/m/d'));
+        }
+        return $this->render('TennisBookAccueilBundle:Accueil:listeAnnonce.html.twig', array('annonces' => $annonces));
     }
 }
