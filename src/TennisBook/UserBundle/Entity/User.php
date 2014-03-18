80c0b2f8b5ec7ed2e62537e1f851d1256a3d632e
yoh91asakura <fix113@hotmail.com>
2014-03-18 13:46:33 +0100

    Corection diverse : -suppresion de fichiers inutiles -corrections mineures

--- src/TennisBook/UserBundle/Entity/User.php
+++ src/TennisBook/UserBundle/Entity/User.php
@@ -8,7 +8,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="fos_user")
+ * @ORM\Table(name="utilisateur")
  */
 class User extends BaseUser
 {
@@ -37,5 +37,38 @@ class User extends BaseUser
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
     }
 }
